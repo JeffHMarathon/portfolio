ade8fc645dd7612b2be692381498f4e366eb75b7
JeffHMarathon <37081304+JeffHMarathon@users.noreply.github.com>
2023-08-30 11:57:13 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,107 +1,110 @@
 var acc = document.getElementsByClassName("accordion");
 var porojectSection = document.getElementById("projects-grid");
+var MorePorojectSection = document.getElementById("more-projects-grid");
 var i;
 
 const projectList = [
     {
       url: "https://www.mscflexone.com",
-      image: "./img/marathon-system.png",
+      image: "./img/marathon-system_50.png",
       title: "The Marathon System",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec augue at tellus convallis efficitur laoreet sed dolor. Morbi metus quam, rutrum vel imperdiet id, aliquet quis mauris. Donec egestas tortor non elementum congue. Integer purus nisl, ultrices vel rhoncus in, eleifend vel risus. Cras placerat ultrices nulla at fringilla. Sed quam diam, tempus in justo quis, varius laoreet lectus. Maecenas iaculis suscipit ligula vel volutpat. Fusce ipsum magna, blandit ac ligula eget, euismod pellentesque enim. "
+      description: "The marathon system has been helping Insurance Agents run their Agency Management and Premium financing for over 30 years. The software is written using Clarion by Softvelocity. The software runs seamlessly in a browser using TSPlus. Key technologies: Windows Hyper-V/Windows Server 2019/Group Policy/Active Directory/ACL’s., AWS S3 Storage/IAM/Bucket and User Policies, Nettalk by Capesoft WebServices/WebPortal/Email/SMS/ftp, IPDriver/IMDD/DFD by Softvelocity, Premier/SendTo by Capesoft, sftp Chilkat Wrapper by Noyantis. "
     },
     {
-      url: "https://www.gomarathon.biz/?_payto_=demo",
-      image: "./img/marathon-application-portal.png",
+      url: "http://www.mscflexone.com/Tutorials/WWW%20-%20Give%20your%20customers%20web%20access%20to%20their%20account.mp4",
+      image: "./img/marathon-application-portal_50.png",
       title: "The Marathon System online portal",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec augue at tellus convallis efficitur laoreet sed dolor. "
+      description: "The Marathon System Online Portal gives customers access to many useful features. The portal not only displays customer data but also allows customers to enter credit card payments, print forms and download documents. The web portal uses Two-factor authentication using either SMS or Email. Key Technologies: Clarion, Nettalk WebServer/Rest/SOAP, AWS S3, Html/CSS/Javascript.  "
     },
     {
       url: "https://khojant.com/",
-      image: "./img/khojant-llc-landing-page.png",
+      image: "./img/khojant-llc-landing-page_50.png",
       title: " khojant llc landing page",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec augue at tellus convallis efficitur laoreet sed dolor. "
-    },
-    {
-      url: "https://jeffhmarathon.github.io/maxstrong-landing-page/",
-      image: "./img/maxstrong-landing-page.png",
-      title: "Landing Page",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec augue at tellus convallis efficitur laoreet sed dolor. "
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/product-landing-page/",
-      image: "./img/product-landing-page.png",
-      title: "Product Page",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/youtube-page/",
-      image: "./img/youtube-page.png",
-      title: "youtube search page",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/slider-section/",
-      image: "./img/quote-slider.png",
-      title: "quote slider",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/pexels-search-page/",
-      image: "./img/pexels-search-page.png",
-      title: "pexel search page",
-      description: ""
-    },                                 
-    {
-      url: "https://jeffhmarathon.github.io/blog-site/",
-      image: "./img/blog-page.png",
-      title: "blog page",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/mario-animation/",
-      image: "./img/mario-animation.png",
-      title: "mario animation",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/tribute-to-boston/",
-      image: "./img/tribute-to-boston.png",
-      title: "tribute to boston",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/technical-documentation-page/",
-      image: "./img/technical-documentation-page.png",
-      title: "technical documentation page",
-      description: ""
-    }, 
-    {
-      url: "https://jeffhmarathon.github.io/survey-form/",
-      image: "./img/survey-form.png",
-      title: "survey form",
-      description: ""
-    },                 
-    {
-      url: "https://jeffhmarathon.github.io/ecommerce-quiz/",
-      image: "./img/ecommerce-quiz.png",
-      title: "ecommerce-quiz",
-      description: ""
-    },                 
-    {
-      url: "https://jeffhmarathon.github.io/order-menu/",
-      image: "./img/order-menu.png",
-      title: "order-menu",
-      description: ""
-    },    
-    {
-      url: "https://jeffhmarathon.github.io/resume-page/",
-      image: "./img/resume-page.png",
-      title: "resume-page",
-      description: ""
-    },        
-
-
+      description: "The khojant LLC landing page. Key technologies: HTML/CSS/Javascript"
+    }
   ]
+
+const moreprojectList = [ 
+  {
+    url: "https://jeffhmarathon.github.io/maxstrong-landing-page/",
+    image: "./img/maxstrong-landing-page_50.png",
+    title: "Landing Page",
+    description: "Landing page for MaxString using responsive design. Key technologies: HTML/CSS/Javascript"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/product-landing-page/",
+    image: "./img/product-landing-page_50.png",
+    title: "Product Page",
+    description: "Simple website for Original Trombones. . Key technologies: HTML/CSS/Youtube Video"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/youtube-page/",
+    image: "./img/youtube-page_50.png",
+    title: "youtube search page",
+    description: "This project page demonstrates accessing the youtube API's using HTML CSS and Javascript"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/pexels-search-page/",
+    image: "./img/pexels-search-page_50.png",
+    title: "pexel search page",
+    description: "This project page demonstrates accessing the pexels API's using HTML CSS and Javascript"
+  },                             
+  {
+    url: "https://jeffhmarathon.github.io/blog-site/",
+    image: "./img/blog-page_50.png",
+    title: "blog page",
+    description: "Plog page project using HTML and CSS"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/technical-documentation-page/",
+    image: "./img/technical-documentation-page_50.png",
+    title: "technical documentation page",
+    description: "Website used for technichal documentation with menu navigation"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/survey-form/",
+    image: "./img/survey-form_50.png",
+    title: "survey form",
+    description: "Website Suvey Form using several form elements"
+  },                 
+  {
+    url: "https://jeffhmarathon.github.io/ecommerce-quiz/",
+    image: "./img/ecommerce-quiz_50.png",
+    title: "ecommerce-quiz",
+    description: "Quiz webpage and results page using javascript, CSS and HTML."
+  },                 
+  {
+    url: "https://jeffhmarathon.github.io/order-menu/",
+    image: "./img/order-menu_50.png",
+    title: "order-menu",
+    description: "Quick order menu using responsive design for Mobile and Desktop. Uses HTML and CSS"
+  },
+  {
+    url: "https://jeffhmarathon.github.io/mario-animation/",
+    image: "./img/mario-animation_50.png",
+    title: "mario animation",
+    description: "This was a fun animation project that uses CSS and HTML"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/tribute-to-boston/",
+    image: "./img/tribute-to-boston_50.png",
+    title: "tribute to boston",
+    description: "This is a webpage for one of the best bands of the 70's, Responsive design using HTML and CSS"
+  }, 
+  {
+    url: "https://jeffhmarathon.github.io/resume-page/",
+    image: "./img/resume-page_50.png",
+    title: "resume-page",
+    description: "A simple resume page using HTML and CSS"
+  },        
+  {
+    url: "https://jeffhmarathon.github.io/slider-section/",
+    image: "./img/quote-slider_50.png",
+    title: "quote slider",
+    description: "This quote slider was a small project to be incorporated into a website. Uses HTML, CSS and Javascript"
+  } 
+]
+
 
 for (i = 0; i < projectList.length; i++) {
   const projectItem = projectList[i];
@@ -109,6 +112,14 @@ for (i = 0; i < projectList.length; i++) {
   const backSection = `<p class="back-panel">${projectItem.description}</p>`
   porojectSection.innerHTML += `<div class="main-panel project">${frontSection}${backSection}</div>` ;
   console.log(porojectSection.innerHTML)
+}
+
+for (i = 0; i < moreprojectList.length; i++) {
+  const projectItem = moreprojectList[i];
+  const frontSection = `<a href="${projectItem.url}" target="_blank" class="front-panel project-tile"><img class="project-image" src="${projectItem.image}" alt="project"/><p class="project-title"><span class="code">&lt;</span>${projectItem.title}<span class="code">&#47;&gt;</span></p></a>`
+  const backSection = `<p class="back-panel">${projectItem.description}</p>`
+  MorePorojectSection.innerHTML += `<div class="main-panel project">${frontSection}${backSection}</div>` ;
+  console.log(MorePorojectSection.innerHTML)
 }
 
 for (i = 0; i < acc.length; i++) {
